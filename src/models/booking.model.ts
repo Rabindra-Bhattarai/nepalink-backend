8df29b347039b459ff6d8ae99fdadadff099f06e
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-11 16:10:22 +0545

    booking model

---
+++ src/models/booking.model.ts
@@ -0,0 +1,26 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+const BookingSchema = new Schema(
+  {
+    memberId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    nurseId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    date: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "declined"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+export interface IBooking extends Document {
+  memberId: mongoose.Types.ObjectId;
+  nurseId: mongoose.Types.ObjectId;
+  date: Date;
+  status: "pending" | "accepted" | "declined";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const BookingModel = mongoose.model<IBooking>("Booking", BookingSchema);
