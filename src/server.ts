63e652b35ab63acee670414e064988df5c088f08
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-11 15:40:09 +0545

    server js

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { connectDatabase } from "./database/mongodb";
 
 connectDatabase();
 
-app.listen(Number(PORT), () => {
+app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server also available on your local IP (e.g., http://172.25.0.222:${PORT})`);
 });
+
