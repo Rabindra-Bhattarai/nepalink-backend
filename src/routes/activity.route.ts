88912f35f2334c2b9e7c16d96a813356b8490c45
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-12 16:03:55 +0545

    activity route

--- src/routes/activity.route.ts
+++ src/routes/activity.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { ActivityController } from "../controllers/activity.controller";
+import { authenticate, isNurse } from "../middlewares/auth.middleware";
 
 const activityController = new ActivityController();
 const activityRouter = Router();
 
-// Log activity
-activityRouter.post("/", (req, res) => activityController.log(req, res));
+// Nurses log activity
+activityRouter.post("/", authenticate, isNurse, (req, res) => activityController.log(req, res));
 
-// Get activities for a booking
-activityRouter.get("/:bookingId", (req, res) => activityController.getByBooking(req, res));
+// Members & Nurses can view activities for a booking
+activityRouter.get("/:bookingId", authenticate, (req, res) => activityController.getByBooking(req, res));
 
 export default activityRouter;
--- src/routes/booking.route.ts
+++ src/routes/booking.route.ts
@@ -1,19 +1,18 @@
 import { Router } from "express";
 import { BookingController } from "../controllers/booking.controller";
+import { authenticate, isMember, isNurse } from "../middlewares/auth.middleware";
 
 const bookingController = new BookingController();
 const bookingRouter = Router();
 
-// Create booking
-bookingRouter.post("/", (req, res) => bookingController.create(req, res));
+// Members create bookings
+bookingRouter.post("/", authenticate, isMember, (req, res) => bookingController.create(req, res));
 
-// Get all bookings (with filters, sorting, pagination)
-bookingRouter.get("/", (req, res) => bookingController.getAll(req, res));
+// Members & Nurses can view bookings
+bookingRouter.get("/", authenticate, (req, res) => bookingController.getAll(req, res));
+bookingRouter.get("/:id", authenticate, (req, res) => bookingController.getById(req, res));
 
-// Get booking by ID
-bookingRouter.get("/:id", (req, res) => bookingController.getById(req, res));
-
-// Update booking status (accept/decline)
-bookingRouter.patch("/:id/status", (req, res) => bookingController.updateStatus(req, res));
+// Nurses update booking status
+bookingRouter.patch("/:id/status", authenticate, isNurse, (req, res) => bookingController.updateStatus(req, res));
 
 export default bookingRouter;
