0f4025cca1b70037e4ff363d5501c59b74ae48a2
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-12 17:18:51 +0545

    admin controller

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { AdminService } from "../services/admin.services";
+import { AdminService } from "../services/admin.service";
 
 const adminService = new AdminService();
 
