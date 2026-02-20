7ffd048c6162f5fe15fcc4b8e2eed2554e31bb2e
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-20 10:38:11 +0545

    server

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,3 @@ app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
   console.log(`Server also available on your local IP (e.g., http://172.25.0.222:${PORT})`);
 });
-
