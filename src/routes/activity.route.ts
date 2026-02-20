34e3f74100d646d39ec1691f618c18459fdb3109
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-20 10:40:09 +0545

    activity routes

--- src/routes/activity.route.ts
+++ src/routes/activity.route.ts
@@ -1,14 +1,11 @@
 import { Router } from "express";
-import { ActivityController } from "../controllers/activity.controller";
 import { authenticate, isNurse } from "../middlewares/auth.middleware";
+import { ActivityController } from "../controllers/activity.controller";
 
-const activityController = new ActivityController();
-const activityRouter = Router();
+const controller = new ActivityController();
+const router = Router();
 
-// Nurses log activity
-activityRouter.post("/", authenticate, isNurse, (req, res) => activityController.log(req, res));
+router.post("/", authenticate, isNurse, controller.log);
+router.get("/:contractId", authenticate, controller.getByContract);
 
-// Members & Nurses can view activities for a booking
-activityRouter.get("/:bookingId", authenticate, (req, res) => activityController.getByBooking(req, res));
-
-export default activityRouter;
+export default router;
