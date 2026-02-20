f394883031a8364de19508cc7f0bfd9dece1591c
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-20 10:39:03 +0545

    activity

--- src/dtos/activity.dto.ts
+++ src/dtos/activity.dto.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const CreateActivityDTO = z.object({
-  bookingId: z.string().min(1, "Booking ID is required"),
+  contractId: z.string().min(1, "Contract ID is required"),
   nurseId: z.string().min(1, "Nurse ID is required"),
   notes: z.string().min(5, "Notes must be at least 5 characters"),
 });
