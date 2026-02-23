72e00b230d82b98a59545a71148ed4d7147f908b
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-23 13:40:54 +0545

    activity service

--- src/services/activity.service.ts
+++ src/services/activity.service.ts
@@ -3,11 +3,20 @@ import { ActivityRepository } from "../repositories/activity.repository";
 const repo = new ActivityRepository();
 
 export class ActivityService {
-  async logActivity(data: any) {
+  async createActivity(data: any) {
     return repo.create(data);
   }
 
-  async getActivitiesForContract(contractId: string) {
-    return repo.findByContractId(contractId);
+  // 🔧 New method to update full activity
+  async updateActivity(id: string, updateData: any) {
+    return repo.updateActivity(id, updateData);
+  }
+
+  async getActivitiesForMember(memberId: string) {
+    return repo.getActivitiesForMember(memberId);
+  }
+
+  async getActivitiesForNurse(nurseId: string) {
+    return repo.getActivitiesForNurse(nurseId);
   }
 }
