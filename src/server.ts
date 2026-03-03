818818e5e9fef1561faa17603a9b14d4b514b44e
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-03-03 14:26:03 +0545

    server

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ connectDatabase();
 // Create HTTP server from Express app
 const httpServer = app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${PORT}`);
-  console.log(`Server also available on your local IP (http://10.221.76.214:${PORT})`);
+  console.log(`Server also available on your local IP (http://10.238.15.214:${PORT})`);
 });
 
 // Attach Socket.IO with multi-origin CORS
