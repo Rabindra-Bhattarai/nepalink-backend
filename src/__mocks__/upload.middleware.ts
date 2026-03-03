9c9587927e8ee68c929c8d193b48a2d9649af3ea
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-03-03 14:26:35 +0545

    upload middleware

---
+++ src/__mocks__/upload.middleware.ts
@@ -0,0 +1,5 @@
+export const uploads = {
+  single: () => (req: any, res: any, next: any) => next(),
+  array: () => (req: any, res: any, next: any) => next(),
+  fields: () => (req: any, res: any, next: any) => next()
+};
